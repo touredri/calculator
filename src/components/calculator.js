d564db8b30bf6319646c50cc928ad6052eabfd8c
touredri <dcompte223@gmail.com>
2023-04-26 22:42:50 +0100

    Calculator setup logic

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,13 +1,35 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 import './calculator.css';
 import CalculatorKey from './calculatorkeys';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calculator">
-    <DisplayResult />
-    <CalculatorKey />
+const Calculator = () => {
+  const [obj, setObj] = useState({ total: null, next: null, operation: null });
+  const handleClick = (buttonName) => {
+    const newObj = calculate(obj, buttonName);
+    setObj(newObj);
+  };
+  return (
+    <div className="calculator">
+      <DisplayResult result={obj.total} next={obj.next} operation={obj.operation} />
+      <CalculatorKey handleClick={handleClick} />
+    </div>
+  );
+};
+
+const DisplayResult = ({ result, next, operation }) => (
+  <div className="calculator__display">
+    <div>{next}</div>
+    {' '}
+    <div>{operation}</div>
+    <div>{result}</div>
   </div>
 );
 
-const DisplayResult = () => <div className="calculator__display">0</div>;
-
+DisplayResult.propTypes = {
+  result: PropTypes.string.isRequired,
+  next: PropTypes.string.isRequired,
+  operation: PropTypes.string.isRequired,
+};
 export default Calculator;
--- src/components/calculatorkeys.js
+++ src/components/calculatorkeys.js
@@ -1,6 +1,7 @@
 import './calculator.css';
+import PropTypes from 'prop-types';
 
-const CalculatorKey = () => (
+const CalculatorKey = ({ handleClick }) => (
   <div className="calculator__keys">
     <table>
       <tr>
@@ -8,7 +9,8 @@ const CalculatorKey = () => (
           <button
             type="button"
             className="calculator__key calculator__key--operator"
-            value="AC"
+            name="AC"
+            onClick={() => handleClick('AC')}
           >
             AC
           </button>
@@ -17,7 +19,8 @@ const CalculatorKey = () => (
           <button
             type="button"
             className="calculator__key calculator__key--operator"
-            value="+/-"
+            name="+/-"
+            onClick={() => handleClick('+/-')}
           >
             +/-
           </button>
@@ -26,7 +29,8 @@ const CalculatorKey = () => (
           <button
             type="button"
             className="calculator__key calculator__key--operator"
-            value="%"
+            name="%"
+            onClick={() => handleClick('%')}
           >
             %
           </button>
@@ -35,7 +39,8 @@ const CalculatorKey = () => (
           <button
             type="button"
             className="calculator__key calculator__key--operator"
-            value="/"
+            name="÷"
+            onClick={() => handleClick('÷')}
           >
             &divide;
           </button>
@@ -43,17 +48,32 @@ const CalculatorKey = () => (
       </tr>
       <tr>
         <td>
-          <button type="button" className="calculator__key" value="7">
+          <button
+            type="button"
+            className="calculator__key"
+            name="7"
+            onClick={() => handleClick('7')}
+          >
             7
           </button>
         </td>
         <td>
-          <button type="button" className="calculator__key" value="8">
+          <button
+            type="button"
+            className="calculator__key"
+            name="8"
+            onClick={() => handleClick('8')}
+          >
             8
           </button>
         </td>
         <td>
-          <button type="button" className="calculator__key" value="9">
+          <button
+            type="button"
+            className="calculator__key"
+            name="9"
+            onClick={() => handleClick('9')}
+          >
             9
           </button>
         </td>
@@ -61,7 +81,8 @@ const CalculatorKey = () => (
           <button
             type="button"
             className="calculator__key calculator__key--operator"
-            value="*"
+            name="x"
+            onClick={() => handleClick('x')}
           >
             &times;
           </button>
@@ -69,17 +90,27 @@ const CalculatorKey = () => (
       </tr>
       <tr>
         <td>
-          <button type="button" className="calculator__key" value="4">
+          <button
+            type="button"
+            className="calculator__key"
+            name="4"
+            onClick={() => handleClick('4')}
+          >
             4
           </button>
         </td>
         <td>
-          <button type="button" className="calculator__key" value="5">
+          <button
+            type="button"
+            className="calculator__key"
+            name="5"
+            onClick={() => handleClick('5')}
+          >
             5
           </button>
         </td>
         <td>
-          <button type="button" className="calculator__key" value="6">
+          <button type="button" className="calculator__key" name="6" onClick={() => handleClick('6')}>
             6
           </button>
         </td>
@@ -87,7 +118,8 @@ const CalculatorKey = () => (
           <button
             type="button"
             className="calculator__key calculator__key--operator"
-            value="-"
+            name="-"
+            onClick={() => handleClick('-')}
           >
             -
           </button>
@@ -95,17 +127,32 @@ const CalculatorKey = () => (
       </tr>
       <tr>
         <td>
-          <button type="button" className="calculator__key" value="1">
+          <button
+            type="button"
+            className="calculator__key"
+            name="1"
+            onClick={() => handleClick('1')}
+          >
             1
           </button>
         </td>
         <td>
-          <button type="button" className="calculator__key" value="2">
+          <button
+            type="button"
+            className="calculator__key"
+            name="2"
+            onClick={() => handleClick('2')}
+          >
             2
           </button>
         </td>
         <td>
-          <button type="button" className="calculator__key" value="3">
+          <button
+            type="button"
+            className="calculator__key"
+            name="3"
+            onClick={() => handleClick('3')}
+          >
             3
           </button>
         </td>
@@ -113,7 +160,8 @@ const CalculatorKey = () => (
           <button
             type="button"
             className="calculator__key calculator__key--operator"
-            value="+"
+            name="+"
+            onClick={() => handleClick('+')}
           >
             +
           </button>
@@ -121,13 +169,23 @@ const CalculatorKey = () => (
       </tr>
       <tr>
         <td colSpan={2}>
-          <button type="button" className="calculator__key zero" value="0">
+          <button
+            type="button"
+            className="calculator__key zero"
+            name="0"
+            onClick={() => handleClick('0')}
+          >
             0
           </button>
         </td>
 
         <td>
-          <button type="button" className="calculator__key" value=".">
+          <button
+            type="button"
+            className="calculator__key"
+            name="."
+            onClick={() => handleClick('.')}
+          >
             .
           </button>
         </td>
@@ -135,7 +193,8 @@ const CalculatorKey = () => (
           <button
             type="button"
             className="calculator__key calculator__key--operator"
-            value="="
+            name="="
+            onClick={() => handleClick('=')}
           >
             =
           </button>
@@ -144,5 +203,9 @@ const CalculatorKey = () => (
     </table>
   </div>
 );
+
+CalculatorKey.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
 
 export default CalculatorKey;
