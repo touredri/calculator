f053124cf228abf06e0d9253c37faccc1818eee2
touredri <dcompte223@gmail.com>
2023-05-04 10:34:52 +0100

    Add test cases

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,19 +1,297 @@
+// import { object } from 'prop-types';
 import calculate from '../logic/calculate';
+// import operate from '../operate';
 
 describe('calculate', () => {
-  test('calculate', () => {
+  it('should reset calculator when buttonName is AC', () => {
     const obj = {
-      total: 3,
-      next: 4,
+      total: '5',
+      next: '3',
       operation: '+',
     };
-    const obj2 = {
+    const result = calculate(obj, 'AC');
+    expect(result).toEqual({
       total: null,
       next: null,
       operation: null,
+    });
+  });
+
+  it('should reset calculator when "AC" button is clicked', () => {
+    const result = calculate({ total: 10, next: 5, operation: '+' }, 'AC');
+    expect(result).toEqual({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  });
+
+  it('should append numbers to "next" when a number button is clicked', () => {
+    const result = calculate({ total: 10, next: '5', operation: '+' }, '2');
+    expect(result).toEqual({
+      total: 10,
+      next: '52',
+      operation: '+',
+    });
+  });
+
+  it('should return an empty object when "0" button is clicked and "next" is "0"', () => {
+    const result = calculate({ total: 10, next: '0', operation: '+' }, '0');
+    expect(result).toEqual({});
+  });
+
+  it('should update "next" when there is an operation and a number button is clicked', () => {
+    const result = calculate({ total: 10, next: '5', operation: '+' }, '2');
+    expect(result).toEqual({
+      total: 10,
+      next: '52',
+      operation: '+',
+    });
+  });
+
+  it('should update "next" and clear the value when there is no operation and a number button is clicked', () => {
+    const result = calculate({ total: 10, next: '5', operation: null }, '2');
+    expect(result).toEqual({
+      total: null,
+      next: '52',
+      operation: null,
+    });
+  });
+
+  it('should add decimal point to "next" when "." button is clicked', () => {
+    const result = calculate({ total: 10, next: '5', operation: null }, '.');
+    expect(result).toEqual({
+      total: null,
+      next: '5.',
+      operation: null,
+    });
+  });
+
+  it('should not add decimal point to "next" if it already has one', () => {
+    const result = calculate({ total: 10, next: '5.5', operation: null }, '.');
+    expect(result).toEqual({
+      total: 10,
+      next: '5.5',
+      operation: null,
+    });
+  });
+
+  it('should add decimal point to "total" when "." button is clicked and "total" is a number', () => {
+    const result = calculate({ total: 10, next: null, operation: null }, '.');
+    expect(result).toEqual({
+      total: '10.',
+      next: null,
+      operation: null,
+    });
+  });
+
+  it('should not add decimal point to "total" if it already has one', () => {
+    const result = calculate(
+      { total: '10.', next: null, operation: null },
+      '.',
+    );
+    expect(result).toEqual({
+      total: '10.',
+      next: null,
+      operation: null,
+    });
+  });
+
+  it('should calculate result when "=" button is clicked', () => {
+    const result = calculate({ total: 10, next: '5', operation: '+' }, '=');
+    expect(result).toEqual({
+      total: '15',
+      next: null,
+      operation: null,
+    });
+  });
+
+  it('should handle the equal sign', () => {
+    const obj = {
+      total: '5',
+      next: '3',
+      operation: '+',
     };
-    const answer = calculate(obj, 'AC');
-    expect(JSON.stringify(answer)).toEqual(JSON.stringify(obj2));
-    expect(calculate({ next: '5' }, 'AC')).toHaveProperty('total', null);
+
+    let result = calculate(obj, '=');
+    expect(result).toEqual({
+      total: '8',
+      next: null,
+      operation: null,
+    });
+
+    result = calculate(result, '+');
+    expect(result).toEqual({
+      total: '8',
+      next: null,
+      operation: '+',
+    });
+
+    result = calculate(result, '4');
+    expect(result).toEqual({
+      total: '12',
+      next: null,
+      operation: '+',
+    });
+  });
+
+  it('should change sign of next or total when +/- is pressed', () => {
+    let obj = {
+      total: '5',
+      next: '3',
+      operation: '+',
+    };
+    let result = calculate(obj, '+/-');
+    expect(result).toEqual({
+      total: '-5',
+      next: '-3',
+      operation: '+',
+    });
+
+    obj = {
+      total: '-5',
+      next: null,
+      operation: null,
+    };
+    result = calculate(obj, '+/-');
+    expect(result).toEqual({
+      total: '5',
+      next: null,
+      operation: null,
+    });
+  });
+
+  it('should handle operation buttons when there is no next', () => {
+    const obj = {
+      total: '5',
+      next: null,
+      operation: '+',
+    };
+
+    let result = calculate(obj, '-');
+    expect(result).toEqual({
+      total: '5',
+      next: null,
+      operation: '-',
+    });
+
+    result = calculate(result, '*');
+    expect(result).toEqual({
+      total: '5',
+      next: null,
+      operation: '*',
+    });
+  });
+
+  it('should handle operation buttons after equal sign', () => {
+    let obj = {
+      total: '5',
+      next: '3',
+      operation: '-',
+    };
+    let result = calculate(obj, '=');
+    expect(result).toEqual({
+      total: '2',
+      next: null,
+      operation: null,
+    });
+
+    result = calculate(result, '+');
+    expect(result).toEqual({
+      total: '2',
+      next: null,
+      operation: '+',
+    });
+
+    result = calculate(result, '4');
+    expect(result).toEqual({
+      total: '6',
+      next: null,
+      operation: '+',
+    });
+
+    obj = {
+      total: '5',
+      next: '3',
+      operation: '-',
+    };
+    result = calculate(obj, '=');
+    expect(result).toEqual({
+      total: '2',
+      next: null,
+      operation: null,
+    });
+
+    result = calculate(result, '*');
+    expect(result).toEqual({
+      total: '2',
+      next: null,
+      operation: '*',
+    });
+
+    result = calculate(result, '4');
+    expect(result).toEqual({
+      total: '8',
+      next: null,
+      operation: '*',
+    });
+  });
+
+  it('should handle operation buttons when there is an existing operation and a next', () => {
+    const obj = {
+      total: '5',
+      next: '3',
+      operation: '+',
+    };
+
+    let result = calculate(obj, '-');
+    expect(result).toEqual({
+      total: '8',
+      next: null,
+      operation: '-',
+    });
+
+    result = calculate(result, '*');
+    expect(result).toEqual({
+      total: '-3',
+      next: null,
+      operation: '*',
+    });
+
+    result = calculate(result, '2');
+    expect(result).toEqual({
+      total: '-6',
+      next: null,
+      operation: '*',
+    });
+  });
+
+  it('should handle operation buttons when there is no existing operation and a next', () => {
+    const obj = {
+      total: '5',
+      next: '3',
+      operation: null,
+    };
+
+    let result = calculate(obj, '+');
+    expect(result).toEqual({
+      total: '8',
+      next: null,
+      operation: '+',
+    });
+
+    result = calculate(result, '-');
+    expect(result).toEqual({
+      total: '8',
+      next: null,
+      operation: '-',
+    });
+
+    result = calculate(result, '/');
+    expect(result).toEqual({
+      total: '8',
+      next: null,
+      operation: '/',
+    });
   });
 });
--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,11 +1,35 @@
 import operate from '../logic/operate';
 
 describe('operate', () => {
-  it('adds two numbers', () => {
-    expect(operate(2, 3, '+')).toBe('5');
+  it('adds two numbers correctly', () => {
+    expect(operate(2, 3, '+')).toEqual('5');
   });
 
-  it('multiple two numbers', () => {
-    expect(operate(2, 3, 'x')).toBe('6');
+  it('subtracts two numbers correctly', () => {
+    expect(operate(5, 3, '-')).toEqual('2');
+  });
+
+  it('multiplies two numbers correctly', () => {
+    expect(operate(2, 3, 'x')).toEqual('6');
+  });
+
+  it('divides two numbers correctly when second number is not zero', () => {
+    expect(operate(6, 3, '÷')).toEqual('2');
+  });
+
+  it("returns 'Can't divide by 0.' when dividing by zero", () => {
+    expect(operate(6, 0, '÷')).toEqual("Can't divide by 0.");
+  });
+
+  it('finds modulo of two numbers correctly when second number is not zero', () => {
+    expect(operate(8, 3, '%')).toEqual('2');
+  });
+
+  it("returns 'Can't find modulo as can't divide by 0.' when dividing by zero", () => {
+    expect(operate(8, 0, '%')).toEqual("Can't find modulo as can't divide by 0.");
+  });
+
+  it('throws an error for unknown operations', () => {
+    expect(() => operate(5, 2, '^')).toThrow("Unknown operation '^'");
   });
 });
